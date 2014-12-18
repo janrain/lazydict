77f407350ce28c10e1d343bcf982e9e25fb93171
Colin von Heuring <colin@janrain.com>
2014-12-17 21:16:25 -0800

    Refactor tests

--- test.py
+++ test.py
@@ -1,42 +1,42 @@
 from unittest import TestCase
-import lazydict
+from lazydict import LazyDictionary, CircularReferenceError, \
+                     ConstantRedefinitionError
 
 class TestLazyDictionary(TestCase):
 
     def test_circular_reference_error(self):
-        d = lazydict.LazyDictionary()
-        d['foo'] = lambda s: s['foo']
-        self.assertRaises(lazydict.CircularReferenceError, d.__getitem__, 'foo')
+        d = LazyDictionary({'foo': lambda s: s['foo']})
+        self.assertRaises(CircularReferenceError, d.__getitem__, 'foo')
 
     def test_constant_redefinition_error(self):
-        d = lazydict.LazyDictionary()
-        d['a'] = 1
-        d['b'] = 2
-        d['sum'] = lambda s: s['a'] + s['b']
+        d = LazyDictionary({
+            'a': 1,
+            'b': 2,
+            'sum': lambda s: s['a'] + s['b'],
+        })
         x = d['sum']
-        self.assertRaises(lazydict.ConstantRedefinitionError, d.__setitem__, 'a', 'hotdog')
-        self.assertRaises(lazydict.ConstantRedefinitionError, d.__delitem__, 'a')
+        self.assertRaises(ConstantRedefinitionError, d.__setitem__, 'a', 'foo')
+        self.assertRaises(ConstantRedefinitionError, d.__delitem__, 'a')
 
     def test_lazy_evaluation(self):
-        d = lazydict.LazyDictionary()
-        d['sum'] = lambda s: s['a'] + s['b']
+        d = LazyDictionary({'sum': lambda s: s['a'] + s['b']})
         d['a'] = 1
         d['b'] = 2
         self.assertEqual(d['sum'], 3)
 
     def test_str(self):
-        d = lazydict.LazyDictionary({'a': 1})
-        self.assertEqual(str(d),   "{'a': 1}")
+        d =        LazyDictionary({'a': 1})
+        self.assertEqual(str(d), "{'a': 1}")
 
     def test_repr(self):
-        d =               lazydict.LazyDictionary({'a': 1})
+        d =                        LazyDictionary({'a': 1})
         self.assertEqual(repr(d), "LazyDictionary({'a': 1})")
 
     def test_fibonacci(self):
         def fib_closure(d, i):
             return lambda d: d[str(i - 2)] + d[str(i - 1)]
 
-        f = lazydict.LazyDictionary({'0': 1, '1': 1})
+        f = LazyDictionary({'0': 1, '1': 1})
         for i in range(2, 100):
             f[str(i)] = fib_closure(f, i)
 
